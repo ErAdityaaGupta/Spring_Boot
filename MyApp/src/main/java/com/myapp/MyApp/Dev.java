08164050299c53ee693fa321949de40965f54ce0
Aditya Gupta <ingenious.adig@gmail.com>
2024-07-18 01:59:05 +0530

    Initial Commit

--- MyApp/src/main/java/com/myapp/MyApp/Dev.java
+++ MyApp/src/main/java/com/myapp/MyApp/Dev.java
@@ -2,6 +2,7 @@ package com.myapp.MyApp;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,15 +11,19 @@ public class Dev {
     @Autowired // it is known as field injection.
    // Laptop laptop = new Laptop();
 
-    Laptop laptop;
+//    Laptop laptop;
 
+    @Qualifier("desktop")
+
+    private Computer comp;
 //    public Dev(Laptop laptop){
 //        this.laptop = laptop;
 //    }
 
     public void build(){
 
-        laptop.compile();
+//        laptop.compile();
+        comp.compile();
 
         System.out.println("working on a project");
     }
