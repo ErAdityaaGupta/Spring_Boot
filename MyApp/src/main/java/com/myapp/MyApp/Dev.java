80bb277d6b932d5dfb1ccf20caeec44f6da7d64d
Aditya Gupta <ingenious.adig@gmail.com>
2024-07-17 13:21:07 +0530

    Initial Commit

---
+++ MyApp/src/main/java/com/myapp/MyApp/Dev.java
@@ -0,0 +1,13 @@
+package com.myapp.MyApp;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("working on a project");
+    }
+
+}
